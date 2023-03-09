f8eb5af084ac4767536099847b8947aeda2ace67
Osamah_Hasan <124389134+Osamah-Hasan@users.noreply.github.com>
2023-03-09 18:27:26 +0000

    update

--- js/script.js
+++ js/script.js
@@ -6,5 +6,5 @@
 
 function myButtonClicked() {
   document.getElementById("multiply-math").innerHTML = "<p>The area is: " + (5 * 3) +"cm&sup2</p>" 
-  document.getElementById("add-multiply-math").innerHTML = "<p>The perimeter is: " + (1 + 5 * 3) +"cm</p>" 
+  document.getElementById("add-multiply-math").innerHTML = "<p>The perimeter is: " + (2 * 5 + 3) +"cm</p>" 
 }
