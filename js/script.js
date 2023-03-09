f363b60e7f40694416596bde8b49d68f1d38f06c
Osamah_Hasan <124389134+Osamah-Hasan@users.noreply.github.com>
2023-03-09 18:08:13 +0000

    update

--- js/script.js
+++ js/script.js
@@ -5,5 +5,6 @@
 // This file contains the JS functions for index.html
 
 function myButtonClicked() {
-  alert("Answer will be supported soon")
+  document.getElementById("multiply-math").innerHTML = "<p>The area is: " + (5 * 3) +"cm&sup2</p>" 
+  document.getElementById("add-multiply-math").innerHTML = "<p>The perimeter is: " + (1 + 5 * 3) +"cm</p>" 
 }
